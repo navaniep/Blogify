9acf93ecc29add93f8db1470741cbfc49a6e0627
navaniep <84153234+navaniep@users.noreply.github.com>
2024-06-14 10:47:53 +0530

    Uploaded project files

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+            About
+        </h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">Hello, I am Pranav Adithya. I am an undergrad of the batch 20-24 from BVRITN.</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">This is a project I have made while learning MERN stack. It is my first full stack application</p>
+        <p className="mx-auto leading-relaxed text-base mb-4">Please email me at pranadithya03@gmail.com if there's anything you would like me to hear.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Hello, Welcome to my Blog</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">This is the home page of my blog.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Feel free to explore and read the latest articles.</p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Don't forget to leave your comments and feedback!</p>
+        </div>
+    )
+}
+
+export default Home     
